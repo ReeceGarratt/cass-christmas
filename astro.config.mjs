76798416c05c481e12c33e430e05027e4215dacf
ReeceGarratt <24543342+ReeceGarratt@users.noreply.github.com>
2025-11-06 21:04:44 +0200

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://cassandragarratt.com',
+  // site: 'https://cassandragarratt.com'
+  site: 'https://reecegarratt.github.io',
   base: '/',
   integrations: [
     tailwind(),
